050a3ec6dff780c2e68745e13be88a5ed875f221
Cherinet <cherinetgobena@yahoo.com>
2019-05-24 06:45:00 +0300

    commit

--- app/Http/Controllers/TestingVueController.php
+++ app/Http/Controllers/TestingVueController.php
@@ -9,7 +9,7 @@ class TestingVueController extends Controller
     public function index()
     {
         return [
-            'name' => 'Cherinet Gobena',
+            'name' => 'Admin',
             ];
     }
 }
